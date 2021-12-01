1286b26a40c515cb267711addbe61d08a1aa8e7f
leogarcru <leo.garcru@gmail.com>
2021-12-01 13:07:39 -0600

    Se agrega funcionalidad a botones AC, 7, 8, return Se agregan escuchas de eventos de click para empezar a mostrar los numeros en el display, y borrar tanto el ultimo escrito, como todo

--- action.js
+++ action.js
@@ -1,1 +1,29 @@
-// alert("Hola");
+let display = "";
+let display_number;
+
+document.addEventListener("DOMContentLoaded", function(event){
+  document.getElementById("seven").addEventListener("click", function(){
+    if(display.length < 9){
+      display += "7";
+    }
+    document.getElementById("bd-display").innerText = display;
+  });
+  
+  document.getElementById("eight").addEventListener("click", function(){
+    if(display.length < 9){
+      display += "8";
+    }
+    document.getElementById("bd-display").innerText = display;
+  });
+
+    document.getElementById("allClear").addEventListener("click", function(){
+    display="";
+    document.getElementById("bd-display").innerText = "0";
+    })
+
+    document.getElementById("return").addEventListener("click", function(){
+      display = display.substring(0, display.length - 1);
+      document.getElementById("bd-display").innerText = display;
+    })
+  }
+)
