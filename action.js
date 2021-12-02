c5266a77942adab9c90abb7a140637d3d50c556d
leogarcru <leo.garcru@gmail.com>
2021-12-01 18:33:06 -0600

    se agrega funcionalidad a todo el teclado numerico todos los numeros 0~9 funcionando se agrega igual funcionalidad al boton , se revisa si ya se ha utilizado la , antes para solo agregar 1 por cada numero

--- action.js
+++ action.js
@@ -8,10 +8,81 @@ document.addEventListener("DOMContentLoaded", function(event){
     }
     document.getElementById("bd-display").innerText = display;
   });
-  
+
   document.getElementById("eight").addEventListener("click", function(){
     if(display.length < 9){
       display += "8";
+    }
+    document.getElementById("bd-display").innerText = display;
+  });
+
+  document.getElementById("nine").addEventListener("click", function(){
+    if(display.length < 9){
+      display += "9";
+    }
+    document.getElementById("bd-display").innerText = display;
+  });
+
+  document.getElementById("four").addEventListener("click", function(){
+    if(display.length < 9){
+      display += "4";
+    }
+    document.getElementById("bd-display").innerText = display;
+  });
+
+  document.getElementById("five").addEventListener("click", function(){
+    if(display.length < 9){
+      display += "5";
+    }
+    document.getElementById("bd-display").innerText = display;
+  });
+
+  document.getElementById("six").addEventListener("click", function(){
+    if(display.length < 9){
+      display += "6";
+    }
+    document.getElementById("bd-display").innerText = display;
+  });
+
+  document.getElementById("three").addEventListener("click", function(){
+    if(display.length < 9){
+      display += "3";
+    }
+    document.getElementById("bd-display").innerText = display;
+  });
+
+  document.getElementById("two").addEventListener("click", function(){
+    if(display.length < 9){
+      display += "2";
+    }
+    document.getElementById("bd-display").innerText = display;
+  });
+
+  document.getElementById("one").addEventListener("click", function(){
+    if(display.length < 9){
+      display += "1";
+    }
+    document.getElementById("bd-display").innerText = display;
+  });
+
+  document.getElementById("zero").addEventListener("click", function(){
+    if(display.length < 9){
+      display += "0";
+    }
+    document.getElementById("bd-display").innerText = display;
+  });
+
+  document.getElementById("dot").addEventListener("click", function(){
+    let puntoPresente = false;
+    if(display.length < 9){
+      for ( let i = 0; i <= display.length; i++){
+        if (display[i] == ".")
+          puntoPresente = true;
+          // break;
+      }
+      if (!puntoPresente){
+        display += ".";
+      }
     }
     document.getElementById("bd-display").innerText = display;
   });
@@ -25,5 +96,8 @@ document.addEventListener("DOMContentLoaded", function(event){
       display = display.substring(0, display.length - 1);
       document.getElementById("bd-display").innerText = display;
     })
-  }
-)
+
+    document.getElementById("cp-button").addEventListener("click", function(){
+      navigator.clipboard.writeText(document.getElementById("bd-display").innerText)
+    })
+})
